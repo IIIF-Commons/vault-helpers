9566dcd316511ed16abdf9b3e61863633b5284ed
Stephen Fraser <stephen.fraser@digirati.com>
2022-10-09 21:34:04 +0100

    Expand objects API

--- src/objects.ts
+++ src/objects.ts
@@ -3,29 +3,108 @@ import { Manifest } from '@iiif/presentation-3';
 import { Reference } from '@iiif/presentation-3/reference';
 
 function defineProperty(name: string, prototype: any, vault: Vault) {
+  prototype[DEFINED] = prototype[DEFINED] || [];
+  prototype[DEFINED].push(name);
+
   Object.defineProperty(prototype, name, {
     get(): any {
-      if (typeof this._refs_[name] === 'undefined') {
+      if (typeof prototype[REFS][name] === 'undefined') {
         return undefined;
       }
 
-      const ref = this._refs_[name];
+      const ref = prototype[REFS][name];
       if (!ref) {
         return ref;
       }
 
-      return wrapObject(vault.get(this._refs_[name]), vault);
+      return wrapObject(vault.get(prototype[REFS][name]), vault);
     },
     set(items: any) {
-      this._refs_[name] = items;
+      const existing = prototype[REFS][name];
+      if (existing !== items) {
+        this[REFS][name] = items;
+
+        // This was a hack, but a much more clever implementation here could make very flexible editing.
+        // For example - manifest.label = "Something" -> manifest.label = {none: "something"}; etc.
+        // Although this might be better in a different library completely.
+        // if (this[REACTIVE]) {
+        //   vault.modifyEntityField({ id: prototype.id, type: prototype.type }, name, unwrapObject(items));
+        // }
+      }
     },
   });
 }
 
-function createPrototype(vault: Vault) {
+const REFS = Symbol.for('_refs_');
+const REACTIVE = Symbol.for('_reactive_');
+const DEFINED = Symbol.for('_defined_');
+
+function createPrototype(vault: Vault, reactive = false) {
   const prototype = {
     id: null,
-    _refs_: {},
+    [DEFINED]: [] as any[],
+    [REFS]: {},
+    [REACTIVE]: null as null | (() => void),
+
+    is(refOrObject: any) {
+      if (typeof refOrObject === 'string') {
+        return this.id === refOrObject;
+      }
+
+      if (refOrObject.id) {
+        return refOrObject.id === this.id;
+      }
+
+      return false;
+    },
+
+    reactive() {
+      if (this[REACTIVE]) {
+        return;
+      }
+
+      this[REACTIVE] = this.subscribe(() => this.refresh(), true);
+
+      return () => {
+        this.unreactive();
+      };
+    },
+
+    refresh() {
+      if (this.id) {
+        const fresh = this.unwrap();
+        for (const key of Object.keys(fresh || {})) {
+          if (this[DEFINED].includes(key)) {
+            (this as any)[REFS][key] = fresh[key as any];
+          } else {
+            (this as any)[key] = fresh[key as any];
+          }
+        }
+      }
+    },
+
+    unreactive() {
+      if (this[REACTIVE]) {
+        this[REACTIVE]();
+        this[REACTIVE] = null;
+      }
+    },
+
+    unwrap() {
+      if (!this.id) {
+        throw new Error('Invalid object');
+      }
+      return vault.get(this.id);
+    },
+
+    toPresentation3() {
+      return vault.toPresentation3(this.unwrap() as any);
+    },
+
+    toPresentation2() {
+      return vault.toPresentation2(this.unwrap() as any);
+    },
+
     toJSON() {
       const that = this as any;
       return {
@@ -62,6 +141,7 @@ function createPrototype(vault: Vault) {
   defineProperty('items', prototype, vault);
   defineProperty('annotations', prototype, vault);
   defineProperty('structures', prototype, vault);
+
   // Linking
   defineProperty('seeAlso', prototype, vault);
   defineProperty('service', prototype, vault);
@@ -85,23 +165,44 @@ function createPrototype(vault: Vault) {
   return prototype;
 }
 
-export function wrapObject(object: any, vault: Vault): any {
+export function unwrapObject(object: any): any {
   if (Array.isArray(object)) {
-    return object.map((o) => wrapObject(o, vault));
+    return object.map((o) => unwrapObject(o)) as any;
   }
 
   if (!object || !object.type) {
     return object;
   }
 
-  const prototype = createPrototype(vault);
+  return { id: object.id, type: object.type };
+}
+
+export function wrapObject(object: any, vault: Vault, reactive = false): any {
+  if (Array.isArray(object)) {
+    return object.map((o) => wrapObject(o, vault, reactive));
+  }
+
+  if (!object || !object.type || !object.id) {
+    return object;
+  }
+
+  const prototype = createPrototype(vault, reactive);
   const newObject = Object.create(prototype);
 
-  return Object.assign(newObject, object) as any;
+  const wrapped =  Object.assign(newObject, object) as any;
+
+  if (reactive) {
+    wrapped.reactive();
+  }
+
+  return wrapped;
 }
 
 export function createObjectsHelper(vault: Vault) {
   return {
+    get(id: string | Reference | string[] | Reference[], reactive = false) {
+      return wrapObject(vault.get(id as any), vault, reactive);
+    },
     async load(id: string | Reference<any>, json?: any): Promise<Manifest> {
       return wrapObject(await vault.load(id, json), vault);
     },
